5d107daa9d28a595fdba6cf63f1516da509cea51
jmk2020bap <jungminkim2020bap@gmail.com>
2025-03-10 16:23:08 +0900

    feat: add common header and button components

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,35 @@
 import "./styles.css";
+import Button from "../Button";
+import Header from "../Header";
 import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <div>Home</div>
+    <div>
+      <Header
+        title={"Home"}
+        leftChild={
+          <Button
+            text={"긍정버튼 "}
+            type="positive"
+            onClick={() => {
+              alert("positive")
+            }}
+          />
+        }
+        rightChild={
+          <Button
+            text={"부정버튼 "}
+            type="negative"
+            onClick={() => {
+              alert("negative")
+            }}
+          />
+        }
+      />
+    </div>
   );
 };
 
