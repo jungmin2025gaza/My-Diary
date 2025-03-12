dcfdf6a4c8af2eb37cb9d13c5e8a46b7720ab5a7
jmk2020bap <jungminkim2020bap@gmail.com>
2025-03-12 16:02:56 +0900

    feat: new page has been added

--- src/components/New/index.jsx
+++ src/components/New/index.jsx
@@ -1,8 +1,34 @@
 import "./styles.css";
+import Button from "../Button";
+import Header from "../Header";
+import Editor from "../Editor";
+import { DiaryDispatchContext } from "../App";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const onSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate("/", { replace: true });
+  };
+
   return (
-    <div>New</div>
+    <div>
+      <Header
+        title="새 일기 쓰기"
+        leftChild={<Button text="< 뒤로 가기" />}
+        onClick={goBack}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
   );
 };
 
